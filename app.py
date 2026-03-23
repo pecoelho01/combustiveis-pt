a9fccff77135d51b66fc2d09198f10dd3936a802
Pedro Coelho <pmnco1@iscte-iul.pt>
2026-03-23 09:46:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ st.title("Combustíveis em Portugal")
 st.markdown("Tabela com os preços médios em Portugal dos combustíveis")
 
 url = "https://api.apiaberta.pt/v1/fuel/prices"
-response = rq.get(url)
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+}
+response = rq.get(url, headers=headers)
 print(response.text) #Debug 
 
 dados = response.json().get('data', [])
