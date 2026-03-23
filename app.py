01e1e4cb09e22ce46bd1823c31be7c85f64da74f
Pedro Coelho <pmnco1@iscte-iul.pt>
2026-03-23 17:56:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,17 @@ dados = response.json().get('data', [])
 data_list = []
 
 for item in dados:
+  
   fuel_name = item.get('fuel_name')
   av_price = item.get('avg_price_eur')
 
-  data_list.append({
-    "Combustível": fuel_name,
-    "Preço médio": av_price
-  })
+  if fuel_name == "Gasóleo de aquecimento" or fuel_name == "Gasóleo colorido" or fuel_name == "Biodiesel B15" or fuel_name == "Gasolina mistura (2 tempos)":
+    continue
+  else: 
+    data_list.append({
+        "Combustível": fuel_name,
+        "Preço médio": av_price
+    })
 
 df_data = pd.DataFrame(data_list)
 st.dataframe(df_data, use_container_width=True)
