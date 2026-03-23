0c3d2982db7609d5fc8e34c81c9ebd376fec98bd
Pedro Coelho <pmnco1@iscte-iul.pt>
2026-03-23 07:52:26 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import requests as rq
+import pandas as pd
+
+st.title("Combustíveis em Portugal")
+
+st.markdown("Tabela com os preços médios em Portugal dos combustíveis")
+
+url = "https://api.apiaberta.pt/v1/fuel/prices"
+response = rq.get(url)
+
+dados = response.json().get('data', [])
+
+data_list = []
+
+for item in dados:
+  fuel_name = item['fuel_name']
+  av_price = item['avg_price_eur']
+
+  data_list.append({
+    "Combustível": fuel_name,
+    "Preço médio": av_price
+  })
+
+  df_data = pd.DataFrame(data_list)
+  st.dataframe(df_data, use_container_width=True)
+
